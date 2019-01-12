eb099c060ee1d82fe39097163dbef1b652af0831
Theodore R. Smith <theodore@phpexperts.pro>
2019-01-12 16:22:18 -0600

    Lots of fixes.

---
+++ tests/integration/Managers/AccountTest.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the Zuora PHP API Client, a PHP Experts, Inc., Project.
+ *
+ * Copyright © 2019 PHP Experts, Inc.
+ * Author: Theodore R. Smith <theodore@phpexperts.pro>
+ *  GPG Fingerprint: 4BF8 2613 1C34 87AC D28F  2AD8 EB24 A91D D612 5690
+ *  https://www.phpexperts.pro/
+ *  https://github.com/phpexpertsinc/Zuora-API-Client
+ *
+ * This file is licensed under the MIT License.
+ */
+
+namespace PHPExperts\ZuoraClient\Tests\Integration\Managers;
+
+use PHPExperts\RESTSpeaker\RESTAuth;
+use PHPExperts\ZuoraClient\Tests\TestCase;
+use PHPExperts\ZuoraClient\ZuoraClient;
+
+class AccountTest extends TestCase
+{
+    /** @var ZuoraClient */
+    protected $api;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $restAuth = new RESTAuth(RESTAuth::AUTH_MODE_PASSKEY);
+        $this->api = new ZuoraClient($restAuth, env('ZUORA_API_HOST'));
+    }
+
+    public function testCanFetchAccountDetails()
+    {
+        $zuoraGUID = '8a80aba7693a825401695a4a53663134';
+        $response = $this->api->account->fetch($zuoraGUID);
+        dd($response);
+    }
+}
