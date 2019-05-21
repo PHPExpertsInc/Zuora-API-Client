ee35f3a802753f149843b94446dedac3f35107ad
Theodore R. Smith <theodore@phpexperts.pro>
2019-05-21 09:00:34 -0500

    Lots and lots of work on Accounts.

--- src/DTOs/Response/Account/BasicInfoDTO.php
+++ src/DTOs/Response/Account/BasicInfoDTO.php
@@ -7,15 +7,18 @@ use PHPExperts\SimpleDTO\SimpleDTO;
 /**
  * See https://www.zuora.com/developer/api-reference/#operation/GET_Account
  *
- * @property string $id
- * @property string $name
- * @property string $accountNumber
- * @property string $notes
- * @property string $status
- * @property string $crmId
- * @property string $batch
- * @property string $invoiceTemplateId
- * @property string $communicationProfileId
+ * @property string      $id
+ * @property string      $name
+ * @property string      $accountNumber
+ * @property string      $notes
+ * @property string      $status
+ * @property string      $crmId
+ * @property string      $batch
+ * @property string      $invoiceTemplateId
+ * @property string      $communicationProfileId
+ * @property null|string $salesRep
+ * @property null|string $parentId
+ * @property null|string $sequenceSetId
  */
 class BasicInfoDTO extends SimpleDTO
 {
--- src/Managers/Account.php
+++ src/Managers/Account.php
@@ -14,10 +14,9 @@
 
 namespace PHPExperts\ZuoraClient\Managers;
 
-use Carbon\Carbon;
-use GuzzleHttp\Psr7\Response;
 use PHPExperts\RESTSpeaker\RESTSpeaker;
-use PHPExperts\ZuoraClient\DTOs\AccountDTO;
+use PHPExperts\ZuoraClient\DTOs\Request\AccountDTO;
+use PHPExperts\ZuoraClient\DTOs\Response\AccountDTO as AccountResponseDTO;
 use PHPExperts\ZuoraClient\ZuoraClient;
 
 class Account extends Manager
@@ -32,16 +31,17 @@ class Account extends Manager
         parent::__construct($zuora, $apiClient);
     }
 
-    public function fetch(string $zuoraGUID)
+    public function fetch(string $zuoraGUID): AccountResponseDTO
     {
         $response = $this->api->get('v1/accounts/' . $zuoraGUID);
+        $this->processResponse($response);
 
-        return $response;
+        return new AccountResponseDTO((array) $response);
     }
 
     public function update(string $zuoraGUID, AccountDTO $accountDTO)
     {
-        $response = $this->api->put('v1/object/account/' . $zuoraGUID, [
+        $response = $this->api->put('v1/accounts/' . $zuoraGUID, [
             'json' => $accountDTO->toArray(),
         ]);
 
