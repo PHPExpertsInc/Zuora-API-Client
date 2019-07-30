b4bd329f14cf1bd6f334daa34d6fa1ef30f33fe0
Theodore R. Smith <theodore@phpexperts.pro>
2019-07-29 23:37:11 -0500

    Added the actual Write\SubscriptionDTO.

--- src/DTOs/Write/Account/SubscriptionDTO.php
+++ src/DTOs/Write/Account/SubscriptionDTO.php
@@ -12,7 +12,7 @@
  * This file is licensed under the MIT License.
  */
 
-namespace PHPExperts\ZuoraClient\DTOs\Write;
+namespace PHPExperts\ZuoraClient\DTOs\Write\Account;
 
 use Carbon\Carbon;
 use PHPExperts\DataTypeValidator\InvalidDataTypeException;
@@ -21,60 +21,28 @@ use PHPExperts\SimpleDTO\WriteOnce;
 use PHPExperts\ZuoraClient\DTOs\Write\RatePlans\RatePlanDTO;
 
 /**
- * Taken from https://www.zuora.com/developer/api-reference/#operation/POST_Subscription
- *
- * @property string           $accountKey            Customer account number or ID.
  * @property Carbon           $contractEffectiveDate
  * @property string           $termType
- * @property int              $renewalTerm
- * @property null|bool        $applyCreditBalance
+ *
  * @property null|bool        $autoRenew
  * @property null|Carbon      $customerAcceptanceDate
- * @property null|Carbon      $documentDate
  * @property null|int         $initialTerm
- * @property null|string      $initialTermPeriodType The default is monthly
  * @property null|string      $invoiceOwnerAccountKey
- * @property null|bool        $invoiceSeparately
- * @property null|Carbon      $invoiceTargetDate
+ * @property null|string      $invoiceSeparately
  * @property null|string      $notes
- * @property null|string      $renewalSetting
- * @property null|string      $renewalTermPeriodType The default is monthly
+ * @property null|int         $renewalTern
  * @property null|Carbon      $serviceActivationDate
  * @property null|RatePlanDTO $subscribeToRatePlans
  * @property null|string      $subscriptionNumber
- * @property null|Carbon      $targetDate
  * @property null|Carbon      $termStartDate
  */
 class SubscriptionDTO extends NestedDTO
 {
     use WriteOnce;
 
-    public const TERM_TYPE_TERMED = 'TERMED';
-    public const TERM_TYPE_EVERGREEN = 'EVERGREEN';
-
     public const TERM_TYPES = [
-        self::TERM_TYPE_TERMED,
-        self::TERM_TYPE_EVERGREEN,
-    ];
-
-    public const TERM_PERIOD_DAY = 'Day';
-    public const TERM_PERIOD_WEEK = 'Week';
-    public const TERM_PERIOD_MONTH = 'Month';
-    public const TERM_PERIOD_YEAR = 'Year';
-
-    public const TERM_PERIODS = [
-        self::TERM_PERIOD_DAY,
-        self::TERM_PERIOD_WEEK,
-        self::TERM_PERIOD_MONTH,
-        self::TERM_PERIOD_YEAR,
-    ];
-
-    public const RENEW_WITH_SPECIFIC_TERM = 'RENEW_WITH_SPECIFIC_TERM';
-    public const RENEW_TO_EVERGREEN = 'RENEW_TO_EVERGREEN';
-
-    public const RENEWAL_TERMS = [
-        self::RENEW_TO_EVERGREEN,
-        self::RENEW_WITH_SPECIFIC_TERM,
+        'TERMED',
+        'EVERGREEN',
     ];
 
     public function __construct(array $input)
@@ -91,10 +59,7 @@ class SubscriptionDTO extends NestedDTO
     protected function extraValidation(array $values)
     {
         $acceptable = [
-            'termType'        => self::TERM_TYPES,
-            'initialTermPeriodType' => self::TERM_PERIODS,
-            'renewalSetting'  => self::RENEWAL_TERMS,
-            'renewalTermPeriodType' => self::TERM_PERIODS,
+            'termType' => self::TERM_TYPES,
         ];
 
         $self = get_class($this);
