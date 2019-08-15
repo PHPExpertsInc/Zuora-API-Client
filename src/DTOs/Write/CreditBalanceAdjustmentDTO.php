ea2a393f15b77d333aeb027af12d419e64e0d620
Kevin Hebert <khebert@texaslawshield.com>
2019-08-15 14:41:03 -0500

    Created CreditBalanceAdjustment DTO
    Now have the ability to create Credit Balance Adjustments (used for applying account balances to invoices or adding to account balances).

---
+++ src/DTOs/Write/CreditBalanceAdjustmentDTO.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace PHPExperts\ZuoraClient\DTOs\Write;
+
+use PHPExperts\DataTypeValidator\DataTypeValidator;
+use PHPExperts\SimpleDTO\SimpleDTO;
+use PHPExperts\SimpleDTO\WriteOnce;
+
+/**
+ * https://www.zuora.com/developer/api-reference/#operation/Object_POSTCreditBalanceAdjustment
+ *
+ * @property null|string $AccountingCode
+ * @property double      $Amount
+ * @property null|string $Comment
+ * @property null|string $ReasonCode
+ * @property null|string $ReferenceId
+ * @property null|string $SourceTransactionId       (Invoice ID, Payment ID, Refund ID) Cannot be null if $SourceTransactionNumber is null
+ * @property null|string $SourceTransactionNumber   (Invoice Number, Payment Number, Refund Number) Cannot be null if $SourceTransactionId is null
+ * @property string      $Type
+ */
+class CreditBalanceAdjustmentDTO extends SimpleDTO
+{
+    use WriteOnce;
+
+    public const CREDIT_BALANCE_ADJUSTMENT_TYPE_INCREASE = 'Increase';
+    public const CREDIT_BALANCE_ADJUSTMENT_TYPE_DECREASE = 'Decrease';
+}
