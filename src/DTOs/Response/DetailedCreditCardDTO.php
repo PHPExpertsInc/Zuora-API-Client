36c3d6b141b71fe6af70f96db4c3b86a106469c9
Theodore R. Smith <theodore@phpexperts.pro>
2019-10-07 01:01:05 -0500

    Fixed a small typo in DetailedCreditCardDTO.

--- src/DTOs/Response/DetailedCreditCardDTO.php
+++ src/DTOs/Response/DetailedCreditCardDTO.php
@@ -20,28 +20,28 @@ use PHPExperts\SimpleDTO\SimpleDTO;
 /**
  * See https://www.zuora.com/developer/api-reference/#operation/Object_GETPaymentMethod
  *
- * @property-read string $Id
- * @property-read string $AccountId
- * @property-read string $Type
- * @property-read bool   $Active
- * @property-read string $PaymentMethodStatus
- * @property-read string $CreditCardHolderName
- * @property-read bool   $IsCompany
- * @property-read string $CreditCardType
- * @property-read string $CreditCardMaskNumber
- * @property-read int    $CreditCardExpirationMonth
- * @property-read int    $CreditCardExpirationYear
- * @property-read string $BankIdentificationNumber
- * @property-read Carbon $LastTransactionDateTime
- * @property-read string $LastTransactionStatus
- * @property-read int    $NumConsecutiveFailures
- * @property-read int    $TotalNumberOfProcessedPayments
- * @property-read int    $TotalNumberOfErrorPayments
- * @property-read bool   $UseDefaultRetryRule
- * @property-read string $CreatedById
- * @property-read Carbon $CreatedDate
- * @property-read string $UpdatedById
- * @property-read Carbon $UpdatedDate
+ * @property-read string  $Id
+ * @property-read string  $AccountId
+ * @property-read string  $Type
+ * @property-read bool    $Active
+ * @property-read string  $PaymentMethodStatus
+ * @property-read string  $CreditCardHolderName
+ * @property-read bool    $IsCompany
+ * @property-read string  $CreditCardType
+ * @property-read string  $CreditCardMaskNumber
+ * @property-read int     $CreditCardExpirationMonth
+ * @property-read int     $CreditCardExpirationYear
+ * @property-read string  $BankIdentificationNumber
+ * @property-read ?Carbon $LastTransactionDateTime
+ * @property-read ?string $LastTransactionStatus
+ * @property-read int     $NumConsecutiveFailures
+ * @property-read int     $TotalNumberOfProcessedPayments
+ * @property-read int     $TotalNumberOfErrorPayments
+ * @property-read bool    $UseDefaultRetryRule
+ * @property-read string  $CreatedById
+ * @property-read Carbon  $CreatedDate
+ * @property-read string  $UpdatedById
+ * @property-read Carbon  $UpdatedDate
  */
 class DetailedCreditCardDTO extends SimpleDTO
 {
