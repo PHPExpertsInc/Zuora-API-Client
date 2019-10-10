b34ed9916e43808363439d06e200fba7b32806c9
Theodore R. Smith <theodore@phpexperts.pro>
2019-10-10 03:24:37 -0500

    Simple extension to the Account DTO.

--- src/DTOs/Read/AccountDTO.php
+++ src/DTOs/Read/AccountDTO.php
@@ -31,7 +31,7 @@ use PHPExperts\ZuoraClient\DTOs\Read\Account\MetricsDTO;
  */
 class AccountDTO extends NestedDTO
 {
-    public function __construct(array $input, array $dtoOptions = null)
+    public function __construct(array $input, array $DTOs = [], array $dtoOptions = null)
     {
         $DTOs = [
             'basicInfo'         => BasicInfoDTO::class,
